50dbab7a06306cc4644750a7242cb0c5fe48a371
GadMinz <baykov97@mail.ru>
2020-11-12 23:46:13 +0300

    hook

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -10,7 +11,7 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <img src={props.profile.photos.large}/>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
         </div>
     )
 }
